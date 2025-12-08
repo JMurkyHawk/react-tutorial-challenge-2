4eef985870273b1103a2d0af8e1522c00f64930e
JMurkyHawk <jmiaguard-github@yahoo.com>
2025-12-08 06:31:17 -0600

    Reset project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import { useState, useEffect } from 'react';
-import Form from './Form';
-import List from './List';
-import Table from './Table';
 
-function App() {
+const App = () => {
     const API_URL = 'https://jsonplaceholder.typicode.com/';
     const [reqType, setReqType] = useState('users');
     const [items, setItems] = useState([]);
 
     useEffect(() => {
+        
+        const fetchItems = () => {
 
-        const fetchItems = async () => {
-            try {
-                const response = await fetch(`${API_URL}${reqType}`);
-                const data = await response.json();
-                setItems(data);
-            } catch (err) {
-                console.log(err);
-            }
         }
 
         fetchItems();
@@ -26,13 +17,9 @@ function App() {
 
     return (
         <div className="App">
-            <Form reqType={reqType} setReqType={setReqType} />
-            <main>
-                {/* <List items={items} /> */}
-                <Table items={items} />
-            </main>
+
         </div>
-    );
+    )
 }
 
 export default App;
